787a9c0da7be6592118b7087840bceee21dd2912
LianneCampbell <lianne.campbell@keyin.com>
2024-05-14 15:19:17 -0230

    Additional Updates

--- Account.java
+++ Account.java
@@ -1,3 +1,62 @@
-public class Account {
+// Lianne Campbell 2024
 
+public class Account {
+    private String id;
+    private String name;
+    private int balance;
+
+    // Constructors
+    public Account(String id, String name) {
+        this.id = id;
+        this.name = name;
+        this.balance = 0;
+    }
+
+    public Account(String id, String name, int balance) {
+        this.id = id;
+        this.name = name;
+        this.balance = balance;
+    }
+
+    // Getter methods
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    // Other methods
+    public int credit(int amount) {
+        balance += amount;
+        return balance;
+    }
+
+    public int debit(int amount) {
+        if (amount <= balance) {
+            balance -= amount;
+        } else {
+            System.out.println("Amount exceeded balance");
+        }
+        return balance;
+    }
+
+    public int transferTo(Account another, int amount) {
+        if (amount <= balance) {
+            debit(amount);
+            another.credit(amount);
+        } else {
+            System.out.println("Amount exceeded balance");
+        }
+        return balance;
+    }
+
+    public String toString() {
+        return "Account[id=" + id + ",name=" + name + ",balance=" + balance + "]";
+    }
 }
--- TestAccount.java
+++ TestAccount.java
@@ -1,3 +1,20 @@
 public class TestAccount {
+    public static void main(String[] args) {
+        // Creating two accounts
+        Account acc1 = new Account("001", "Acc1", 5000);
+        Account acc2 = new Account("002", "Acc2", 4000);
 
+        // Displaying balance of both accounts
+        System.out.println("\nCurrent Balance:");
+        System.out.println("Balance of Acc1: $" + acc1.getBalance());
+        System.out.println("Balance of Acc2: $" + acc2.getBalance());
+
+        // Transferring $1000.00 from account 1 to account 2
+        acc1.transferTo(acc2, 1000);
+
+        // Displaying balance of both accounts again
+        System.out.println("\nAfter transfer:");
+        System.out.println("Balance of Acc1: $" + acc1.getBalance());
+        System.out.println("Balance of Acc2: $" + acc2.getBalance());
+    }
 }
