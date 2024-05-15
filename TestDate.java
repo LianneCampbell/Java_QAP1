42446cbec8f81995182b0ab8a478378d10dd8341
LianneCampbell <lianne.campbell@keyin.com>
2024-05-15 09:03:32 -0230

    Update all Files

--- TestDate.java
+++ TestDate.java
@@ -1,3 +1,9 @@
 public class TestDate {
+    public static void main(String[] args) {
+        // Creating a Date object
+        Date date = new Date(14, 5, 2024);
 
+        // Printing the date using toString() method
+        System.out.println("Today As Formatted Date: " + date.toString());
+    }
 }
--- TestTime.java
+++ TestTime.java
@@ -1,3 +1,15 @@
 public class TestTime {
+    public static void main(String[] args) {
+        // Creating two Time objects
+        Time t1 = new Time(21, 10, 15);
+        Time t2 = new Time(10, 20, 25);
 
+        // Calling nextSecond() for t1 and previousSecond() for t2
+        t1.nextSecond();
+        t2.previousSecond();
+
+        // Displaying the final times for t1 and t2
+        System.out.println("Final time for t1(Next Second): " + t1.toString());
+        System.out.println("Final time for t2(Previous Second): " + t2.toString());
+    }
 }
