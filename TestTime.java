b91ec7313de3fbce5bfdad6139599105e2bcd32d
LianneCampbell <lianne.campbell@keyin.com>
2024-05-16 10:12:29 -0230

    Final Changes

--- TestTime.java
+++ TestTime.java
@@ -4,6 +4,10 @@ public class TestTime {
         Time t1 = new Time(21, 10, 15);
         Time t2 = new Time(10, 20, 25);
 
+        // Displaying the first times for t1 and t2
+        System.out.println("First time for t1: " + t1.toString());
+        System.out.println("First time for t2: " + t2.toString());
+
         // Calling nextSecond() for t1 and previousSecond() for t2
         t1.nextSecond();
         t2.previousSecond();
